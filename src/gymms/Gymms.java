44744aee66c9d50a8e4bcee59206cc8e0fc27858
MazenMusleh <mazen11299@gmail.com>
2022-03-16 00:01:15 +0200

    v0.1 Gym

--- src/gymms/Gymms.java
+++ src/gymms/Gymms.java
@@ -26,6 +26,7 @@ public class Gymms {
             new login().setVisible(true);
             
             System.out.println("hello");
+            System.out.println("Shefoo");
         }
     }
 }
