537f955f17956ecb22615fd500f8e5c871835866
sherif emad <sherifemad53@gmail.com>
2022-04-28 15:58:12 +0200

    v0.4

---
+++ src/utils/utilsFunctions.java
@@ -0,0 +1,9 @@
+package utils;
+
+abstract public class utilsFunctions {
+    
+    //todo check functions in this class and all functions that doesn't need to be in databse manager
+    //like get date 
+    
+    
+}
