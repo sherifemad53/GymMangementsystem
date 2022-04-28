2a077f48fa13176bd3a07bf7e47728b6a3b8f7e5
jehadkh <m.jehad.kh@gmail.com>
2022-04-28 16:09:03 +0200

    my first commit14q

--- src/utils/utilsFunctions.java
+++ src/utils/utilsFunctions.java
@@ -4,6 +4,8 @@ abstract public class utilsFunctions {
     
     //todo check functions in this class and all functions that doesn't need to be in databse manager
     //like get date 
-    
+    void hello(){
+        System.out.println("oee");
+    }
     
 }
